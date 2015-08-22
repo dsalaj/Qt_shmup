41b9ab282e8a44afbdc934852d4522f2ae27c5aa
Darjan Salaj <salaj@student.tugraz.at>
2015-08-22 16:22:06 +0200

    added global speed_factor to constants

--- Qt_shmup/constants.h
+++ Qt_shmup/constants.h
@@ -3,12 +3,14 @@
 
 #include <random>
 
-const int player_speed = 4;
-const int enemy_spawn_timeout = 2000;
+const float speed_factor = 1.4;
 
-const unsigned int speed_e01 = 2;
-const unsigned int speed_e02 = 1;
-const unsigned int speed_eb01 = 2;
+const int player_speed = 4 * speed_factor;
+const int enemy_spawn_timeout = 2000 * speed_factor;
+
+const unsigned int speed_e01 = 2 * speed_factor;
+const unsigned int speed_e02 = 1 * speed_factor;
+const unsigned int speed_eb01 = 2 * speed_factor;
 
 const unsigned int health_e01 = 1;
 const unsigned int health_e02 = 4;
@@ -18,8 +20,8 @@ const unsigned int score_e01 = 1;
 const unsigned int score_e02 = 2;
 const unsigned int score_eb01 = 20;
 
-const unsigned int bullet_speed_01 = 10;
-const unsigned int bullet_speed_02 = 4;
-const unsigned int bullet_speed_e01 = 6;
+const unsigned int bullet_speed_01 = 10 * speed_factor;
+const unsigned int bullet_speed_02 = 4 * speed_factor;
+const unsigned int bullet_speed_e01 = 6 * speed_factor;
 #endif // CONSTANTS
 
